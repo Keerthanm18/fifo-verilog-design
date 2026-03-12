02d4b5527b778dd6de6d2e6bd57dc31338525229
Keerthan M <mailmekeerthanm@gmail.com>
2026-03-12 17:36:33 +0530

    Refactor fifo-testbench for syntax and clarity
    Fixed syntax errors and improved display messages in the testbench.

--- fifo-testbench.v
+++ fifo-testbench.v
@@ -15,25 +15,28 @@ clk_i = 1'b0;
 forever #5 clk_i = ~clk_i;
 end
 
-
+initial begin
 rst_i = 1;
-@(posedge clk_i)
 wr_en_i = 0;
 rd_en_i = 0;
 wdata_i = 0;
+
+@(posedge clk_i)
 
 rst_i = 0;
 
 repeat(5) begin
 @(posedge clk_i)
 if(full_o == 0)begin
-wr_en = 1;
+wr_en_i = 1;
 wdata_i = $urandom_range(5,50);
-$dispaly("wr_en = %0b,wdata_i = %0b",wr_en,wdata_i,full_o);
+$display("wr_en_i = %0b,wdata_i = %0b,full_o = %0b",wr_en_i,wdata_i,full_o);
 end
 else begin
 wr_en_i = 0;
-$display("NO WRITE OPERATION")
+$display("NO WRITE OPERATION");
+end
+end
 
 @(posedge clk_i)
 wr_en_i = 0;
@@ -43,12 +46,13 @@ wdata_i = 0;
 repeat(5) begin
 @(posedge clk_i)
 if(empty_o == 0)begin
-rd_en = 1;
-$dispaly("rd_en_i = %0b,rdata_i = %0b",rd_en_i,rdata_i,empty_o);
+rd_en_i = 1;
+$display("rd_en_i = %0b,rdata_o = %0b,empty_o = %0b",rd_en_i,rdata_o,empty_o);
 end
 else begin
 rd_en_i = 0;
-$display("NO READ OPERATION")
+$display("NO READ OPERATION");
+end
 end
 
 rst_i = 1;
@@ -58,8 +62,8 @@ $finish;
 end
 
 initial begin
-$dump.file(dump.vcd);
-$dump.vars(1)
+$dumpfile("dump.vcd");
+$dumpvars(1);
 end
 
 endmodule
